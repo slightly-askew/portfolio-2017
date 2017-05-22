aa4558085f8354044db016291edb542c779f5ea4
Slightly <oaskew@gmail.com>
2017-05-22 18:32:52 +1000

    Add fronts to mainTheme.type

---
+++ src/global/style/mainTheme.js
@@ -0,0 +1,8 @@
+export default {
+  Type: {
+    Title: '"Fira Sans", Helvetica, sans-serif',
+    Heading: '"Montserrat", Roboto, sans-serif',
+    Paragraph: '"Karla", Helvetica, sans-serif',
+    Label: '"Fira Mono", monospace'
+  }
+};
