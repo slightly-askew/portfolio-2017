fb74fee342f0f62d0e4a093de3ccdee18c2cf311
Slightly <oaskew@gmail.com>
2017-05-23 18:46:10 +1000

    Title and header styling

--- src/features/header/header-main/styled-components/Title.js
+++ src/features/header/header-main/styled-components/Title.js
@@ -1,19 +1,18 @@
 //@flow
 
-import styled from "styled-components";
-import { baseAdjust } from "../utils";
+import styled, { css } from "styled-components";
 
 export default styled.h1`
 
-  text-align: center;
-  font-size: 1.5em;
-  line-height: 1;
-  font-family: 'Fira Sans', sans-serif;
-  font-weight: 300;
-  margin: 0;
-  color: white;
-  cursor: pointer;
-  ${baseAdjust(2)}
-  flex: 1 1 content-fit;
+  ${({ theme }: { theme: () => string }) => css`
 
+    ${theme.type.title};
+    text-align: center;
+    font-size: 4em;
+    line-height: 1;
+    margin: 0;
+    color: #131313;
+    cursor: pointer;
+
+  `}
 `;
--- src/features/header/header-main/styled-components/Wrapper.js
+++ src/features/header/header-main/styled-components/Wrapper.js
@@ -7,7 +7,7 @@ export default styled.div`
   box-sizing: border-box;
   position: relative;
   width: 100%;
-  background-color: #000;
+  background-color: #eee;
   padding-top: 0.5em;
 
   @media (min-width: 400px) {
