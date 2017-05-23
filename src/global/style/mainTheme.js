b9aa18377823b8bac03ae122894e1a920356cc35
slightly-askew <oaskew@gmail.com>
2017-05-24 08:53:30 +1000

    add font-features to title in theme

--- src/global/style/mainTheme.js
+++ src/global/style/mainTheme.js
@@ -6,6 +6,7 @@ export default {
       font-family: "Fira Sans", Helvetica, sans-serif;
       font-weight: 300;
       letter-spacing: -0.04em;
+      font-feature-settings: "kern", "liga", "clig", "calt", "dlig";
       ${baseAdjust(2)};
     `,
     heading: '"Montserrat", Roboto, sans-serif',
