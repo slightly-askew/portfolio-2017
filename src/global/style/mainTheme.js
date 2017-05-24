736265070f7bdf9a2f222d38e86fe9df32bb5489
Slightly <oaskew@gmail.com>
2017-05-24 15:50:22 +1000

    feat: mainTheme scales and typography settings
    Black and white tones added; requires colors when decided

--- src/global/style/mainTheme.js
+++ src/global/style/mainTheme.js
@@ -1,4 +1,5 @@
 import { baseAdjust } from "./utils";
+import { modularScale } from "polished";
 
 export default {
   type: {
@@ -6,13 +7,48 @@ export default {
       font-family: "Fira Sans", Helvetica, sans-serif;
       font-weight: 300;
       letter-spacing: -0.04em;
+      font-variant-ligatures: common-ligatures contextual discretionary-ligatures;
       font-feature-settings: "kern", "liga", "clig", "calt", "dlig";
       ${baseAdjust(2)};
     `,
-    heading: '"Montserrat", Roboto, sans-serif',
-    paragraph: '"Karla", Helvetica, sans-serif',
-    label: '"Fira Mono", monospace'
+    heading: `
+      font-family: "Montserrat:700", Roboto, sans-serif;
+      font-weight: 700;
+      letter-spacing: 0.04em;
+      font-variant-ligatures: common-ligatures contextual discretionary-ligatures;
+      font-feature-settings: "kern", "liga", "clig", "calt", "dlig";
+      ${baseAdjust(0.4)};
+    `,
+    paragraph: `
+      font-family: "Karla:400", Helvetica, sans-serif;
+      font-weight: 700;
+      letter-spacing: 0.04em;
+      font-feature-settings: "kern", "liga", "clig", "calt", "onum", "pnum";
+    `,
+    label: `
+      font-family: "Fira Mono:400", monospace';
+      font-weight: 700;
+      letter-spacing: 0.04em;
+      font-variant-ligatures: common-ligatures contextual discretionary-ligatures;
+      font-feature-settings: "kern", "liga", "clig", "calt", "dlig";
+      ${baseAdjust(0.2)};
+    `
   },
 
-  scales: {}
+  scales: {
+    majSecond: (n: number = 1): ((?*) => string) =>
+      modularScale(n, "1em", "majorSecond"),
+    majThird: (n: number = 1): ((?*) => string) =>
+      modularScale(n, "1em", "majThird"),
+    perfFourth: (n: number = 1): ((?*) => string) =>
+      modularScale(n, "1em", "perfectFourth"),
+    perfFifth: (n: number = 1): ((?*) => string) =>
+      modularScale(n, "1em", "perfectFifth")
+  },
+
+  color: {
+    background: "#fafafa",
+    black: "#0b0b0b",
+    primary: "blue"
+  }
 };
