1f1e11b2dc48f254b70871ffef56fdf18fa48d0a
Slightly <oaskew@gmail.com>
2017-05-25 17:35:48 +1000

    Add Main snapshot and update snaps

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Main } from "./scenes/Main";
 
+import renderer from "react-test-renderer";
+
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Main />, div);
+});
+
+it("renders a snapshot", () => {
+  const tree = renderer.create(<Main />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
