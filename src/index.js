d6a6ba8659b72f88de0df7649eaaa5eeb3bf0467
Slightly <oaskew@gmail.com>
2017-05-25 15:59:41 +1000

    Remove type declaration for module

--- src/index.js
+++ src/index.js
@@ -4,24 +4,17 @@ import ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
 import { Route } from "react-router";
-import { ConnectedRouter as Router } from "react-router-redux";
-
+import { ConnectedRouter } from "react-router-redux";
 import store, { history } from "./store";
 
 import App from "./App";
 
-declare var module: {
-  hot: {
-    accept(path: Class<React$Component<*, *, *>>, callback: () => void): void
-  }
-};
-
-const render = Component => {
+const render = (Component: any) => {
   return ReactDOM.render(
     <Provider store={store}>
-      <Router history={history}>
+      <ConnectedRouter history={history}>
         <Route path="/" component={Component} />
-      </Router>
+      </ConnectedRouter>
     </Provider>,
     document.getElementById("root")
   );
@@ -30,7 +23,7 @@ const render = Component => {
 render(App);
 
 if (module.hot) {
-  module.hot.accept(App, () => {
+  module.hot.accept("./App", () => {
     const NextApp = require("./App").default;
     render(NextApp);
   });
