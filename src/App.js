b4466bf9068b0d1515a646df091e447f3fa68c96
Slightly <oaskew@gmail.com>
2017-05-25 15:49:03 +1000

    Remove global styles and add some dummy state (cards)

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as actionCreators from "./data/actions/actionCreators";
-import insertGlobalStyles from "./global/style/globalStyles";
 
 import { Main } from "./scenes/Main";
 
-function mapStateToProps(): {} {
-  return {};
+function mapStateToProps(state: { cards: {}[] }): {} {
+  return { cards: state.cards };
 }
 
 export function mapDispatchToProps(dispatch: {}) {
   return bindActionCreators(actionCreators, dispatch);
 }
-
-insertGlobalStyles();
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
