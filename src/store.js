a8e85105fe985f9fbc3fbd1e93701876fb0f449d
Slightly <oaskew@gmail.com>
2017-05-25 12:46:09 +1000

    Add redux logger and hot reloading to store

--- src/store.js
+++ src/store.js
@@ -4,16 +4,33 @@ import { createStore, compose, applyMiddleware } from "redux";
 import { routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
 import reducers from "./data/reducers";
+import cards from "./data/state/cards";
 
-const defaultState = {};
-
+const defaultState = { cards };
 export const history = createHistory();
+
+const middlewares = [routerMiddleware(history)];
+
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+
+  middlewares.push(logger);
+}
 
 const enhancers = compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f,
-  applyMiddleware(routerMiddleware(history))
+  applyMiddleware(...middlewares)
 );
 
 const store = createStore(reducers, defaultState, enhancers);
 
-export default store;
+export default function configureStore() {
+  const store = createStore(reducers, defaultState, enhancers);
+
+  if (module.hot) {
+    module.hot.accept(reducers, () => {
+      const nextRootReducer = require("./data/reducers").default;
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+}
