687b8066a908dc6a9bbd4c184e143981dec5493a
Slightly <oaskew@gmail.com>
2017-05-26 12:35:57 +1000

    Remove media query that accounds for status bar
    Testing if the necessary space will be added by the mobile browser automatically
    TODO: verify no additional padding required.

--- src/features/header/header-main/styled-components/Wrapper.js
+++ src/features/header/header-main/styled-components/Wrapper.js
@@ -10,10 +10,6 @@ export const Wrapper = styled.div`
     position: relative;
     width: 100%;
     background-color: ${theme.color.background};
-    padding-top: ${theme.scale.s4(-1)};
-
-    @media (min-width: 675px) {
-      padding-top: 0;
-    }
+    
   `}
 `;
