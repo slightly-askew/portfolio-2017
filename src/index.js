3131241c275988034a95ea8e7a2ccacd07fc043e
Slightly <oaskew@gmail.com>
2017-05-27 13:26:32 +1000

    Change Route component to import from react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
-import { Route } from "react-router";
+import { Route } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
 import store, { history } from "./store";
 
