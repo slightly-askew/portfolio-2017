2be103fb33a5f48a4cfe9053fd121f383426dfa1
Slightly <oaskew@gmail.com>
2017-05-30 15:00:09 +1000

    Skewify Title in Heading-main

--- src/features/header/header-main/index.js
+++ src/features/header/header-main/index.js
@@ -4,10 +4,17 @@ import React from "react";
 import { Title, Wrapper } from "./styled-components";
 import { DesktopNav } from "./nav-main/desktop";
 import { LinkedInLogo } from "../../../elements/linkedin-icon";
+import { Highlight } from "../../../elements/highlight";
+import { Skewify } from "../../../elements/skewify";
+import theme from "../../../global/style/mainTheme";
 
 export const HeaderMain = (props: {}): React$Element<*> => (
   <Wrapper>
-    <Title>Oliver Askew</Title>
+    <Title>
+      <Highlight url="/" highlightColor={theme.color.yellow}>
+        Oliver <Skewify>Askew</Skewify>
+      </Highlight>
+    </Title>
     <DesktopNav {...props} />
     <LinkedInLogo {...props} />
   </Wrapper>
