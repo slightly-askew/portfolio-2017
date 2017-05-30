aad30a696dc88e5370d814f335d4ecdc2720b213
Slightly <oaskew@gmail.com>
2017-05-30 15:02:50 +1000

    Add MemoryRouter component to tests to mock route path (resolves error)

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,25 @@ import ReactDOM from "react-dom";
 import { Main } from "./scenes/Main";
 
 import renderer from "react-test-renderer";
+import { MemoryRouter } from "react-router";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Main />, div);
+  ReactDOM.render(
+    <MemoryRouter initialEntries={["/"]} initialIndex={0}>
+      <Main />
+    </MemoryRouter>,
+    div
+  );
 });
 
 it("renders a snapshot", () => {
-  const tree = renderer.create(<Main />).toJSON();
+  const tree = renderer
+    .create(
+      <MemoryRouter initialEntries={["/"]} initialIndex={0}>
+        <Main />
+      </MemoryRouter>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/features/header/__tests__/index.js
+++ src/features/header/__tests__/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Header } from "../index";
 import renderer from "react-test-renderer";
+import { MemoryRouter } from "react-router";
 
 test("header renders correctly", () => {
-  const component = renderer.create(<Header />);
+  const component = renderer.create(
+    <MemoryRouter initialEntries={["/"]} initialIndex={0}>
+      <Header />
+    </MemoryRouter>
+  );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
