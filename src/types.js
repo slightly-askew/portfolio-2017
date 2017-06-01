12e8e23c3d1b410dafa23260872a37ffb2de89c1
Slightly <oaskew@gmail.com>
2017-06-01 15:43:41 +1000

    Add Buuble types to global libdef

--- src/types.js
+++ src/types.js
@@ -45,3 +45,66 @@ declare var module: {
     accept(path: string, callback: () => void): void
   }
 };
+ //Bubble
+declare type originTuple = [number, number];
+
+declare type dataObject = {
+  cx: number,
+  cy: number,
+  r: number,
+  origin: originTuple
+};
+
+declare type defaultBubbleConfig = {
+  children?: Array<string | Array<string | object<string>>>,
+  characterWidth: number,
+  minTextWidth: number,
+  paddingLrg: number,
+  paddingSml: number,
+  textHeight: number,
+  verticalMargin: number,
+  columns: number,
+  size: string,
+  underlineAdjustment: number,
+  speachDirection: String<"top-left"
+      | "top-right"
+      | "right-top"
+      | "right-bottom"
+      | "bottom-left"
+      | "bottom-right"
+      | "left-bottom"
+      | "bottom-right">,
+  dimentions: {
+    x: number,
+    y: number
+  },
+  origin: {
+    x: number,
+    y: number
+  }
+};
+
+declare type optionalBubbleConfig = {
+  children?: Array<string | [string, { target: string }]>,
+  characterWidth?: number,
+  minTextWidth?: number,
+  paddingLrg?: number,
+  paddingSml?: number,
+  textHeight?: number,
+  verticalMargin?: number,
+  columns?: number,
+  size?: string,
+  underlineAdjustment?: number,
+  speachDirection?: String<"top-left"
+      | "top"
+      | "top-right"
+      | "right-top"
+      | "right"
+      | "right-bottom"
+      | "bottom-left"
+      | "bottom"
+      | "bottom-right"
+      | "left-bottom"
+      | "bottom"
+      | "bottom-right">
+};
