c742e023da85dee39bcf01c11b9b7f56ef67d1ff
Slightly <oaskew@gmail.com>
2017-06-07 12:36:59 +1000

    Include Bubble dropdown in navigation

--- src/features/header/header-main/navigation/index.js
+++ src/features/header/header-main/navigation/index.js
@@ -6,6 +6,7 @@ import { LinkedInIcon } from "../../../../elements/linkedin-icon";
 import { OctocatIcon } from "../../../../elements/octocat-icon";
 import { Highlight } from "../../../../elements/highlight";
 import { Chevron } from "../../../../elements/chevron";
+import Bubble from "../../../../elements/bubble";
 import theme from "../../../../global/style/mainTheme";
 
 export const MainNav = (props: {}) => (
@@ -16,10 +17,16 @@ export const MainNav = (props: {}) => (
       <Li>
         <Highlight highlightColor="transparent" {...props}>
           <Chevron {...props}>Download</Chevron>
+          <Bubble
+            {...props}
+            textItems={[
+              { label: "Resume", target: "#" },
+              { label: "Portfoliontopolov", target: "#" }
+            ]}
+          />
         </Highlight>
       </Li>
       <Li><Highlight {...props}>Contact</Highlight></Li>
-
     </Ul>
     <Ul>
       <Li><LinkedInIcon /></Li>
