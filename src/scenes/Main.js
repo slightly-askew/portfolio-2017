67172fb3209fe3fe14198682c80c21dcffcb70cc
Slightly <oaskew@gmail.com>
2017-06-11 13:02:31 +1000

    Add flow annotation to Main

--- src/scenes/Main.js
+++ src/scenes/Main.js
@@ -1,7 +1,9 @@
+//@flow
+
 import React from "react";
 import { Header } from "../features/header";
 import insertGlobalStyles from "../global/style/globalStyles";
 
 insertGlobalStyles();
 
-export const Main = props => <Header {...props} />;
+export const Main = (props: { [mixed]: mixed }) => <Header {...props} />;
