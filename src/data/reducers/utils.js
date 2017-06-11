34641e0084e5047081caa4b6dc0ff79455fe6df8
Slightly <oaskew@gmail.com>
2017-06-11 12:56:56 +1000

    Add utility functions to reducers
    Create reducer (removes need for switch statemenet), updateObject and updateObjectInArray

---
+++ src/data/reducers/utils.js
@@ -0,0 +1,26 @@
+//@flow
+
+export const updateObject = (
+  oldObj: { [string]: any },
+  newVals: { [string]: any }
+): { [string]: any } => Object.assign({}, oldObj, newVals);
+
+export const updateObjectInArray = (
+  array: mixed[],
+  id: number,
+  updateCallback: () => mixed
+): mixed[] => [
+  ...array.slice(0, id),
+  updateCallback(array[id]),
+  ...array.slice(id + 1)
+];
+
+export const createReducer = (
+  initialState: any,
+  handlers: { [string]: <T>(*) => T }
+) => (state: any = initialState, action: { type: string }): mixed => {
+  if (handlers.hasOwnProperty(action.type)) {
+    return handlers[action.type](state, action);
+  }
+  return state;
+};
