ec11d4ec02a8f1304bde2fee29464f2cc6b00d5f
Slightly <oaskew@gmail.com>
2017-06-16 10:04:30 +1000

    Type props for Header

--- src/features/header/index.js
+++ src/features/header/index.js
@@ -3,11 +3,24 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import mainTheme from "../../global/style/mainTheme";
+import { detectWidth } from "./componentQueries";
 
 import { HeaderMain } from "./header-main";
 
-export const Header = ({ ...props }: {}) => (
-  <ThemeProvider theme={mainTheme}>
-    <HeaderMain {...props} />
-  </ThemeProvider>
-);
+type props = {
+  openMobileNav: () => mixed,
+  closeMobileNav: () => mixed,
+  ui: {
+    mobileMenuIsOpen: boolean
+  }
+};
+
+const HeaderMainCQ = detectWidth(HeaderMain);
+
+export const Header = (props: props) => {
+  return (
+    <ThemeProvider theme={mainTheme}>
+      <HeaderMainCQ {...props} />
+    </ThemeProvider>
+  );
+};
