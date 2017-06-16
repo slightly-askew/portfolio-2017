df12c0945c9c0ac8015c324ad0d93e8923a8b961
Slightly <oaskew@gmail.com>
2017-06-16 10:16:07 +1000

    Add top positioning to chevron and reformat with Prettify

--- src/elements/chevron/styled-components/Svg.js
+++ src/elements/chevron/styled-components/Svg.js
@@ -3,23 +3,34 @@
 import styled from "styled-components";
 
 export const Svg = styled.svg.attrs({
+  className: "chevron__svg",
   viewBox: "0 0 24 24"
 })`
-  ${({ theme, isActive, stateful }: { theme: Theme, isActive: boolean, stateful: boolean }): string => `
+  ${({
+    theme,
+    isActive,
+    stateful
+  }: {
+    theme: Theme,
+    isActive: boolean,
+    stateful: boolean
+  }): string => `
 
     position: relative;
     height: ${theme.scale.s1(2)};
     width: ${theme.scale.s1(2)};
-    ${theme.utils.baseAdjust(6)}
+    top: ${theme.scale.s1(-9)};
     padding-left: ${theme.scale.s8(-2)};
     transform-origin: 60% 60%;
     will-change: transform fill;
     transition: all 0.2s ease-in-out;
-    ${isActive && stateful ? `
+    ${isActive && stateful
+      ? `
       fill: ${theme.effect.darken(0.2, theme.color.black)};
       transform: rotate(180deg);
 
-    ` : ``}
+    `
+      : ``}
 
   `}
 `;
