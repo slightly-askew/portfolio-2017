833c66dd18ea68ee38d94a69c5348a5db27de0ed
Slightly <oaskew@gmail.com>
2017-06-16 15:26:06 +1000

    Update HeaderMain to work with container query

--- src/features/header/header-main/index.js
+++ src/features/header/header-main/index.js
@@ -1,22 +1,36 @@
 //@flow
 
-import React from "react";
+import React, { Component } from "react";
+import isEqual from "lodash/isEqual";
 import { Title, Wrapper } from "./styled-components";
 import { MainNav } from "./navigation";
 import { Highlight } from "../../../elements/highlight";
 import { Skewify } from "../../../elements/skewify";
+import { MobileMenuButton } from "../../../elements/mobile-menu-button";
 import theme from "../../../global/style/mainTheme";
 import addActiveState from "../../../global/hoc/addActiveState";
 
+type props = {
+  openMobileNav: () => mixed,
+  closeMobileNav: () => mixed,
+  ui: {
+    mobileMenuIsOpen: boolean,
+    breakpoints: {
+      main: string
+    }
+  }
+};
+
 const ActiveTitle = addActiveState(Title);
 
-export const HeaderMain = (props: {}) => (
+export const HeaderMain = (props: props) =>
   <Wrapper>
+    {props.ui.breakpoints.main.width === "mobile" &&
+      <MobileMenuButton {...props} />}
     <ActiveTitle>
       <Highlight url="/" highlightColor="transparent">
         Oliver <Skewify>Askew</Skewify>
       </Highlight>
     </ActiveTitle>
-    <MainNav {...props} />
-  </Wrapper>
-);
+    {props.ui.breakpoints.main.width === "desktop" && <MainNav {...props} />}
+  </Wrapper>;
