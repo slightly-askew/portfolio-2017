a81d514d914c2f330fb562674261e6ac93dd9308
Slightly <oli@oaskew.co.uk>
2017-07-02 14:56:26 +1000

    Include main-nav feature in mobile-menu and adjust background style

--- src/features/mobile-menu/index.js
+++ src/features/mobile-menu/index.js
@@ -6,9 +6,13 @@ import { Background } from "./styled-components";
 import { componentQuery } from "./componentQueries";
 import wrapWithComponentQuery from "../../global/hoc/wrapWithComponentQuery";
 import { compose, onlyUpdateForKeys } from "recompose";
+import { MainNav } from '../../elements/navigation-main';
 
 type props = {};
 
 const enhance = compose(componentQuery, wrapWithComponentQuery("mobileMenu"));
 
-export const MobileMenu = enhance((props: props) => <Background />);
+export const MobileMenu = enhance((props: props) => (
+  <Background>
+    <MainNav {...props}/>
+  </Background>));
