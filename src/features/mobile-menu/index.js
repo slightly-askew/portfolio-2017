7569dbb138913a3d811436369e51561c7eb18f6d
Slightly <oli@oaskew.co.uk>
2017-07-02 19:57:52 +1000

    Only render mobile nav when mobileNavIsOpen

--- src/features/mobile-menu/index.js
+++ src/features/mobile-menu/index.js
@@ -6,13 +6,14 @@ import { Background } from "./styled-components";
 import { componentQuery } from "./componentQueries";
 import wrapWithComponentQuery from "../../global/hoc/wrapWithComponentQuery";
 import { compose, onlyUpdateForKeys } from "recompose";
-import { MainNav } from '../../elements/navigation-main';
+import { MainNav } from "../../elements/navigation-main";
 
 type props = {};
 
 const enhance = compose(componentQuery, wrapWithComponentQuery("mobileMenu"));
 
-export const MobileMenu = enhance((props: props) => (
+export const MobileMenu = enhance((props: props) =>
   <Background>
-    <MainNav {...props}/>
-  </Background>));
+    {props.ui.mobileMenuIsOpen && <MainNav {...props} />}
+  </Background>
+);
