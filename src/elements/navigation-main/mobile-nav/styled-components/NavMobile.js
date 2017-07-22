62cf081425e8f68a169e9d66e16373c501552c66
Slightly <oli@oaskew.co.uk>
2017-07-22 10:30:57 +1000

    Start building out main nav file structure to accomodate ease of styling

---
+++ src/elements/navigation-main/mobile-nav/styled-components/NavMobile.js
@@ -0,0 +1,28 @@
+//@flow
+
+import styled, { css } from "styled-components";
+
+const Nav = styled.nav`
+  opacity: 1;
+  background: transparent;
+  z-index: 10;
+  display: flex;
+  flex-grow: 1;
+  justify-content: space-between;
+  position: relative;
+
+  ${({ theme, ui }) => css`
+    ${ui.breakpoints.main.device === "desktop" &&
+      css`
+      display: inline-flex;
+      padding-left: 1em;
+    `}
+    ${ui.breakpoints.main.device === "mobile" &&
+      css`
+      flex-direction: column;
+      align-items: baseline;
+    `}
+  `};
+`;
+
+export default Nav;
---
+++ src/elements/navigation-main/mobile-nav/styled-components/index.js
@@ -0,0 +1,5 @@
+//@flow
+
+//export {default as ListTypes} from './ListTypes';
+export { default as NavMobile } from "./NavMobile";
+//export {default as SubNav} from './ListTypes';
